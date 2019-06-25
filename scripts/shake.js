bb5d46c0d75c0151eacdafb3995c760fdbe479b0
kevinikos <kevin.nikolas123@gmail.com>
2019-06-25 17:11:41 +0200

    Add shake effect on each form input

--- scripts/shake.js
+++ scripts/shake.js
@@ -1,10 +1,36 @@
+// checkbox
+const checkboxStatus = document.getElementById("permission");
 const checkbox = document.querySelector(".checkbox");
 
-const shakeCheckbox = () => {
-  checkbox.classList.add("shake_checkbox");
-  setTimeout(() => {
-    checkbox.classList.remove("shake_checkbox");
-  }, 500);
+const shakeCheckbox = e => {
+  if (checkboxStatus.checked === false) {
+    checkbox.classList.add("shake_element");
+    setTimeout(() => {
+      checkbox.classList.remove("shake_element");
+    }, 500);
+    e.preventDefault();
+    return false;
+  }
+  return true;
 };
 
-checkbox.addEventListener("click", shakeCheckbox);
+// inputs
+const inputs = document.querySelectorAll("input, textarea");
+const sendBtn = document.getElementById("sendBtn");
+
+const shakeElements = e => {
+  inputs.forEach(input => {
+    if (input.value === "") {
+      input.classList.add("shake_element");
+      setTimeout(() => {
+        input.classList.remove("shake_element");
+      }, 500);
+      e.preventDefault();
+      return false;
+    }
+  });
+  shakeCheckbox();
+  return true;
+};
+
+sendBtn.addEventListener("click", shakeElements);
