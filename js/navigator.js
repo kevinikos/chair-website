54b5b71ca8c0d0d03bebe349640f7e92cefb1a7b
kevinikos <kevin.nikolas123@gmail.com>
2019-07-15 22:49:51 +0200

    Add button which scroll to top of the website

--- js/navigator.js
+++ js/navigator.js
@@ -3,6 +3,22 @@ const navigator = () => {
   const contactSection = document.querySelector(".contact_section");
   const priceButton = document.getElementById("priceBtn");
   const priceSection = document.querySelector(".price_section");
+  const scrollUpButton = document.getElementById("scrollUpBtn");
+
+  window.addEventListener("scroll", () => {
+    if (document.documentElement.scrollTop > 100) {
+      scrollUpButton.classList.add("visible");
+    } else {
+      scrollUpButton.classList.remove("visible");
+    }
+  });
+
+  scrollUpButton.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
 
   contactButton.addEventListener("click", e => {
     e.preventDefault();
